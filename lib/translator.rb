c8e91f695dddeea30080def75c567e26892dd0ad
kimia kamrava <kimiakamrava93@gmail.com>
2020-09-23 19:20:21 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -12,6 +12,7 @@ def load_library(emoticon_file)
   emoticons.each do |meaning, value|
     english = value[0]
     japanese = value[1]
+    binding.pry
     emoticon_lib['get_meaning'][japanese] = meaning
     emoticon_lib['get_emoticon'][english] = japanese
   end
