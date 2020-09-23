0a3d9f1d615c034b4143308d2b2ab058a49994f9
kimia kamrava <kimiakamrava93@gmail.com>
2020-09-23 19:34:45 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -8,7 +8,7 @@ require 'pry'
   
 def load_library(emoticon_file)
   emoticons = YAML.load_file(emoticon_file)
-  emoticon_lib = {'get_meaning' => {}, 'get_emoticon' => {}}
+  
   emoticons.each do |meaning, value|
     english = value[0]
     japanese = value[1]
