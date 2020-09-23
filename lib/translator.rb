9a38205480307ba94b49279bb21e07760b2ff042
kimia kamrava <kimiakamrava93@gmail.com>
2020-09-23 18:42:07 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,11 +3,11 @@ require 'pry'
 require 'yaml'
 
 def load_library
-  emoticon_file = YAML.load_file('./lib/emoticons.yml')
+  emoticon_file = YAML.load_file('lib/emoticons.yml')
   emoticon =  '(>゜))))彡)'
   
 def load_library(emoticon_file)
-  emoticons = YAML.load_file('./lib/emoticons.yaml')
+  emoticons = YAML.load_file('lib/emoticons.yaml')
   emoticons_lib = {'get_meaning' => {}, 'get_emoticon' => {}}
   emoticons.each do |meaning, value|
     english = value[0]
