3aa9c5d0ec31a59c445e46a5c7531c914e121219
kimia kamrava <kimiakamrava93@gmail.com>
2020-09-23 19:54:02 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -22,6 +22,7 @@ emoticon_lib
 end
 
 
+
 def get_japanese_emoticon(emoticon_file , emoticon)
   emoticon_lib =load_library(emoticon_file)
   japanese_emoticon = emoticon_lib['get_meaning'][emoticon]
