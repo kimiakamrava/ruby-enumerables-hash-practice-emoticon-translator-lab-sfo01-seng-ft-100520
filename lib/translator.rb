ef07cae478062b18012e410f2f1a502a3445f7b2
kimia kamrava <kimiakamrava93@gmail.com>
2020-09-23 18:21:21 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,11 +1,18 @@
 require 'pry'
 # require modules here
-require "yaml"
-path = YAML.load_file('lib/emoticons.yml')
+require 'yaml'
 
-def load_library
-  emoticons = {"get meaning" => {}, "get emoticon" => {}}
-  binding.pry 
+
+def load_library(emoticon_file)
+  emoticons = YAML.load_file('./lib/emoticons.yaml')
+  emoticons_lib = {"get meaning" => {}, "get emoticon" => {}}
+  emoticons.each do |meaning, value|
+    english = value[0]
+    japanese = value[1]
+    emoticon_lib['get_meaning']['japanese'] = meaning
+    emoticon_lib['get_emoticon']['english'] = japanese
+  end
+emoticon_lib  
 end
 
   
